d2e3a1551f05b51a7949b43ef9f9a89c3717945c
LAPTOP-USEO3RG7\Disha <devaiya86@gmail.com>
2017-06-12 11:11:13 -0600

    Added web application structure

---
+++ src/main/java/restservices/repositoryBean/RepositoryProduct.java
@@ -0,0 +1,74 @@
+
+package restservices.repositoryBean;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+    "error",
+    "id",
+    "result"
+})
+@XmlRootElement
+public class RepositoryProduct {
+
+    @JsonProperty("error")
+    private Object error;
+    @JsonProperty("id")
+    private String id;
+    @JsonProperty("result")
+    private Result result;
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+    @JsonProperty("error")
+    public Object getError() {
+        return error;
+    }
+
+    @JsonProperty("error")
+    public void setError(Object error) {
+        this.error = error;
+    }
+
+    @JsonProperty("id")
+    public String getId() {
+        return id;
+    }
+
+    @JsonProperty("id")
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @JsonProperty("result")
+    public Result getResult() {
+        return result;
+    }
+
+    @JsonProperty("result")
+    public void setResult(Result result) {
+        this.result = result;
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+}
